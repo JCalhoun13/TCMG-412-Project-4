891bd322d9e0fb7abe6de342b40e1c5726ec8922
kwalksosa <113211885+kwalksosa@users.noreply.github.com>
2022-09-26 20:43:56 -0500

    Add files via upload
    makes sure we don't pull the same request twice.

---
+++ duplicateRequests.py
@@ -0,0 +1,16 @@
+url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+regex = re.compile(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*")
+
+print('Check to see log already exists:')
+
+if  not path.exists('log'):
+	print('Log did not exist, downloading: ')
+	print(url)
+	urllib.request.urlretrieve(url, 'log')
+	print('Finished Downloading')
+else:
+	print("Log existed, skipping .\n")
+
+file = open('log', 'r')
+
+logfile =[]
